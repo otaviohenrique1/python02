77a87614215805411566fab36957320edf6078ee
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-11 14:42:41 -0300

    commit 5

--- sabor-express/modelos/restaurante.py
+++ sabor-express/modelos/restaurante.py
@@ -1,21 +1,41 @@
 class Restaurante:
-    nome = ""
-    categoria = ""
-    ativo = False
-    # ativo: bool = False
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        # ativo: bool = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f"{self._nome} | {self._categoria}"
+
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(
+                f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}"
+            )
+
+    # @property => modificar como aquele atributo vai ser lido
+    @property
+    def ativo(self):
+        return "⌧" if self._ativo else "☐"
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 
-restaurante_praca = Restaurante()
-# restaurante_praca.nome = "Praça"
-# restaurante_praca.categoria = "Gourmet"
-
-
-restaurante_pizza = Restaurante()
-
-
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-
+# restaurante_praca = Restaurante("Praça", "Gourmet")
+# restaurante_praca.alternar_estado()
+# restaurante_pizza = Restaurante("Pizza Express", "Italiana")
+# restaurantes = [restaurante_praca, restaurante_pizza]
 # print(dir(restaurante_praca))
 # print(vars(restaurante_praca))
-print(restaurante_praca.ativo)
+# print(vars(restaurante_pizza))
+# print(restaurante_praca)
+# print(restaurante_pizza)
+# print(restaurante_praca.ativo)
+# Restaurante.listar_restaurantes()
